38f8676fb7b37783768bbcf528543407961aa792
Ankiwoong <ankiwoong@gmail.com>
2020-07-03 11:25:28 +0900

    snippets url mapping

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,6 +4,7 @@ from snippets import views
 
 urlpatterns = [
     path("users/", views.UserList.as_view()),
+    path("snippets/", views.SnippetList.as_view()),
     path("users/<int:pk>/", views.UserDetail.as_view()),
 ]
 
